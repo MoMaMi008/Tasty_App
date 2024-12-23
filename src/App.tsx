1df57eaaea3534dfed91ea64769a39f4f1c1670e
Moritz Reinhold <reinhold.moritz887@gmail.com>
2024-12-23 10:15:28 +0100

    add: Router in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import "./App.css";
 
 function App() {
-    return <></>;
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <>
+                <Route path="/" element={} />
+                <Route path="/:category" element={}>
+                    <Route path="/:category/:meal" element={} />
+                </Route>
+            </>
+        )
+    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
